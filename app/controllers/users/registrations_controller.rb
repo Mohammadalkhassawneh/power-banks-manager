175f5fdc7d7c135f1912e0aad418a9d2ea77f08a
mohammadalkhassawneh <mohammadmalkhassawneh@gmail.com>
2024-06-14 22:54:43 +0300

    Fix session handling for Devise for registration endpoint

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,23 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionsFix
   respond_to :json
 
   private
 
   def sign_up_params
     params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        message: 'Signed up successfully.',
+        user: resource
+      }, status: :ok
+    else
+      render json: {
+        message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"
+      }, status: :unprocessable_entity
+    end
   end
 end
