dd9b5c96ebe69bc446f7b8359caff054fe3dd9b7
mohammadalkhassawneh <mohammadmalkhassawneh@gmail.com>
2024-06-15 01:17:05 +0300

    Add validation to power bank to prevent create power bank assigned to full capacity station

--- app/models/power_bank.rb
+++ app/models/power_bank.rb
@@ -2,4 +2,16 @@ class PowerBank < ApplicationRecord
   belongs_to :station, optional: true
   belongs_to :warehouse, optional: true
   belongs_to :user, optional: true
+
+  validates :serial_number, uniqueness: true
+
+  validate :validate_station_capacity
+
+  private
+
+  def validate_station_capacity
+    if station && station.power_banks.count >= 10
+      errors.add(:station_id, "cannot have more than 10 power banks")
+    end
+  end
 end
